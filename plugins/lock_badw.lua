8e2b483d13018c06c0fa781bc330ed5c7c1259a0
aaabanana <navidspamkiller@gmail.com>
2016-05-05 05:15:05 -0700

    Update lock_badw.lua

--- plugins/lock_badw.lua
+++ plugins/lock_badw.lua
@@ -1,24 +1,25 @@
+antilink = {}-- An empty table for solving multiple kicking problem
+
+do
 local function run(msg, matches)
-if msg.to.type == 'chat' then
-    if is_momod then
-        return
-    end
-    local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)] then
-        if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['lock_badw'] then
-                lock_adds = data[tostring(msg.to.id)]['settings']['lock_badw']
-            end
-        end
-    end
-    local chat = get_receiver(msg)
-    local user = "user#id"..msg.from.id
-    if lock_adds == "yes" then
-        send_large_msg(chat, 'Badwords is not allowed here!!')
-        chat_del_user(chat, user, ok_cb, true)
-    end
+  if is_momod(msg) then -- Ignore mods,owner,admins
+    return
+  end
+local data = load_data(_config.moderation.data)
+if data[tostring(msg.to.id)]['settings']['lock_badw'] then
+if data[tostring(msg.to.id)]['settings']['lock_badw'] == 'yes' then
+if antilink[msg.from.id] == true then
+delete_msg(msg.id, ok_cb, true)
+return
+end
+delete_msg(msg.id, ok_cb, true)
+send_large_msg("channel#id"..msg.to.id, 'فحش ممنوعه اوی تو'..msg.from.username.. ' !') 
+antilink[msg.from.id] = true
 end
 end
+end
+local function cron()
+antichat = {} -- Clear antichat table
 return {
   patterns = {
     "(ک*س)$",
